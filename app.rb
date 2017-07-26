1d6f6a1d484bddb794c0f66574e097d889483ad2
Ty-Zeta <tyofzeta@gmail.com>
2017-07-26 13:51:34 -0400

    asks hair color

--- app.rb
+++ app.rb
@@ -15,22 +15,44 @@ get '/age' do
 end
 
 post '/age' do
-  age = params[:user_age]
   name = params[:user_name]
-  redirect '/three_numbers?user_name=' + name + '&user_age=' + age
+  age = params[:user_age]
+  redirect '/hair?user_name=' + name + '&user_age=' + age
 end
 
-get '/three_numbers' do
-  age = params[:user_age]
+get '/hair' do
   name = params[:user_name]
-  erb :three_numbers, :locals => {:name => name, :age => age}
+  age = params[:user_age]
+  erb :get_hair, :locals => {:name => name, :age => age}
 end
 
-post '/three_numbers' do
-    name = params[:user_name]
-    age = params[:user_age]
-    favnum1 = params[:user_number_1]
-    favnum2 = params[:user_number_2]
-    favnum3 = params[:user_number_3]
-    redirect '/total?user_name=' + name + '&user_age=' + age + '&user_number_1=' + favnum1 + '&user_number_2=' + favnum2 + '&user_number_3=' + favnum3
-  end
+post '/hair' do
+  name = params[:user_name]
+  age = params[:user_age]
+  hair = params[:user_hair]
+  redirect '/get_food?user_name=' + name + '&user_age=' + age + '&user_hair=' + hair
+end
+
+# get '/three_numbers' do
+#   age = params[:user_age]
+#   name = params[:user_name]
+#   erb :three_numbers, :locals => {:name => name, :age => age}
+# end
+
+# post '/three_numbers' do
+#     name = params[:user_name]
+#     age = params[:user_age]
+#     favnum1 = params[:user_number_1]
+#     favnum2 = params[:user_number_2]
+#     favnum3 = params[:user_number_3]
+#     redirect '/total?user_name=' + name + '&user_age=' + age + '&user_number_1=' + favnum1 + '&user_number_2=' + favnum2 + '&user_number_3=' + favnum3
+# end
+#
+# get '/total' do
+#   name = params[:user_name]
+#   age = params[:user_age]
+#   favnum1 = params[:user_number_1]
+#   favnum2 = params[:user_number_2]
+#   favnum3 = params[:user_number_3]
+#   erb :total, :locals => {:name => name, :age => age, }
+# end
