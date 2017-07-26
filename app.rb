ba5fecf0174a9e11c5301dcf528d5847a2331d46
Ty-Zeta <tyofzeta@gmail.com>
2017-07-26 14:55:46 -0400

    asks for 3 #s

--- app.rb
+++ app.rb
@@ -81,26 +81,32 @@ post '/drink' do
   redirect '/three_numbers?user_name=' + name + '&user_age=' + age + '&user_hair=' + hair + '&user_food=' + food + '&user_drink' + drink
 end
 
-# get '/three_numbers' do
-#   age = params[:user_age]
-#   name = params[:user_name]
-#   erb :three_numbers, :locals => {:name => name, :age => age}
-# end
-#
-# post '/three_numbers' do
-#     name = params[:user_name]
-#     age = params[:user_age]
-#     favnum1 = params[:user_number_1]
-#     favnum2 = params[:user_number_2]
-#     favnum3 = params[:user_number_3]
-#     redirect '/total?user_name=' + name + '&user_age=' + age + '&user_number_1=' + favnum1 + '&user_number_2=' + favnum2 + '&user_number_3=' + favnum3
-# end
-#
-# get '/total' do
-#   name = params[:user_name]
-#   age = params[:user_age]
-#   favnum1 = params[:user_number_1]
-#   favnum2 = params[:user_number_2]
-#   favnum3 = params[:user_number_3]
-#   erb :total, :locals => {:name => name, :age => age, }
-# end
+get '/three_numbers' do
+  name = params[:user_name]
+  age = params[:user_age]
+  hair = params[:user_hair]
+  food = params[:user_food]
+  drink = params[:user_drink]
+  erb :three_numbers, :locals => {:name => name, :age => age, :hair => hair, :food => food, :drink => drink}
+end
+
+post '/three_numbers' do
+    name = params[:user_name]
+    age = params[:user_age]
+    hair = params[:user_hair]
+    food = params[:user_food]
+    drink = params[:user_drink]
+    favnum1 = params[:user_number_1]
+    favnum2 = params[:user_number_2]
+    favnum3 = params[:user_number_3]
+    redirect '/total?user_name=' + name + '&user_age=' + age + '&user_number_1=' + favnum1 + '&user_number_2=' + favnum2 + '&user_number_3=' + favnum3
+end
+
+get '/total' do
+  name = params[:user_name]
+  age = params[:user_age]
+  favnum1 = params[:user_number_1]
+  favnum2 = params[:user_number_2]
+  favnum3 = params[:user_number_3]
+  erb :total, :locals => {:name => name, :age => age, }
+end
